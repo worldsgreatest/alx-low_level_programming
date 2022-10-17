364c8508ca9a14cd9ac8f61bd4e6fb77b1a1bdc0
worldsgreatest <annettebitherman2@gmail.com>
2022-10-17 16:05:56 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- prints the output
+ * Return: 0
+ **/
+int main(void)
+{
+	char s;
+
+	for (s = '0'; s <= '9'; s++)
+	{
+		putchar(s);
+	}
+	putcahr('\n');
+	return (0);
+}
