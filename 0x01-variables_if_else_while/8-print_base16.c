881ca4a9420ee83e75b12a4ddde467e8caf9b2b7
worldsgreatest <annettebitherman2@gmail.com>
2022-10-17 16:51:27 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- prints output
+ * return: 0
+ **/
+int main(void)
+{
+	int d;
+	char hexvalues[] = "0123456789abcdef";
+
+	for (d = 0; d < 16; d++)
+	{
+		putchar(hexvalues[d]);
+	}
+	putchar('\n');
+	return (0);
+}
