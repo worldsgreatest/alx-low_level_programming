4607adbe8964740d572f0839cac69bce68d8925d
worldsgreatest <annettebitherman2@gmail.com>
2022-10-19 06:01:45 -0700

    Task o

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main- prints _putchar
+ * Return: 0
+ **/
+int main(void)
+
+	_putchar('_');
+	_putchar('m');
+	_putchar('s');
+	_putchar('r');
+	_putchar('c');
+	_putchar('q');
+	_putchar('u');
+	_putchar('a');
+	_putchar('\n');
+
+	return (0);
