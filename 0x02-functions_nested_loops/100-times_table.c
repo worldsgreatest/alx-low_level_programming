8455ca49f32659d2531e8c0e10d066a0f3fcfe6d
worldsgreatest <annettebitherman2@gmail.com>
2022-10-19 17:27:46 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,7 +27,7 @@ void print_times_table(int n)
 				else if (op <= 99)
 				{
 					_putchar(32);
-					_putchar((op / 10) +48);
+					_putchar((op / 10) + 48);
 					_putchar((op % 10) + 48);
 				}
 				else
