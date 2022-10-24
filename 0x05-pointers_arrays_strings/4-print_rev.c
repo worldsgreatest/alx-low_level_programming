e4d0b6bd6e495ab0199e62cc4957a780a18be361
worldsgreatest <annettebitherman2@gmail.com>
2022-10-24 14:43:58 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - prints astring in reverse
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int n = 0;
+	while (s[n] != '\0')
+		n++;
+	for (n = n -1; n >= 0; n--)
+		_putchar(s[n]);
+	
+	_putchar('\n');
+}
