f2066227ae7a0fecbdf101186c0d6a1b36fc74d2
worldsgreatest <annettebitherman2@gmail.com>
2022-10-24 15:09:00 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: input
+ * Return: string in reverse
+ */
+void rev_string(char *s)
+{
+	char rv =(s[0]);
+	int c = 0;
+	int n;
+
+	while (s[c] != '\0')
+		c++;
+	for (n = 0; n < c; n++)
+	{
+		c--;
+		rv = s[n];
+		s[n] = s[c];
+		s[c] = rv;
+	}
+}
