a1e2116f0401243a83f5f138dec4755e42b0c14f
worldsgreatest <annettebitherman2@gmail.com>
2022-11-01 15:33:10 -0700

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn -  gets the length of a prefix substring
+ * @s: string
+ * @accept: prifix that needs to be measured
+ * Return: number of byte
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int b = 0;
+	int a;
+
+	while (*s)
+	{
+		for (a = 0; accept[a]; a++)
+		{
+			if (*s == accept[a])
+			{
+				b++;
+				break;
+			}
+			else if (accept [a + 1] == '\0')
+				return (b);
+		}
+		s++;
+	}
+	return (b);
+}
