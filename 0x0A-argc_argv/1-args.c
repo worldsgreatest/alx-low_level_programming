6804d21bd52fed783c5b51a19311b777e00b55eb
worldsgreatest <annettebitherman2@gmail.com>
2022-11-09 15:34:21 -0800

    1 updated

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * @argv: command listed
  * Return: if successful return 0
  */
-int main(int argc, char *argv __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
