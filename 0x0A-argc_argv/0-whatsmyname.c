cd7de0b93ae8ace059f11f2c39c4340d8264ce5a
worldsgreatest <annettebitherman2@gmail.com>
2022-11-09 07:26:38 -0800

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name followed by a new line
+ * @argc: the argc parameter
+ * @argv: an array of command listed
+ * Return: return 0 if successful
+ */
+int main (int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+}
