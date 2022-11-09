8011ee01cdeba25f1bbc0f2f457942fa0103da4e
worldsgreatest <annettebitherman2@gmail.com>
2022-11-09 08:23:12 -0800

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,8 @@
  * @argv: an array of command listed
  * Return: return 0 if successful
  */
-int main (int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
+	return (0);
 }
