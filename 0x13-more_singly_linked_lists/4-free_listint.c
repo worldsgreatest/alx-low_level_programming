af8db3d2b10c0a2fe740b3bbd61919b42f3380ad
worldsgreatest <annettebitherman2@gmail.com>
2022-11-30 15:23:22 -0800

    Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint -  frees a listint_t list
+ * @head: head of a list
+ *
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
+}
