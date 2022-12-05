0ccf6f3a7367d38ada0c970c5266ddc465607e48
worldsgreatest <annettebitherman2@gmail.com>
2022-12-05 15:50:21 -0800

    task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -119,7 +119,7 @@ void print_version(unsigned char *e_ident)
 {
 	printf("  Version:                           %d",
 		e_ident[EI_VERSION]);
-	
+
 	switch (e_ident[EI_VERSION])
 	{
 		case EV_CURRENT:
